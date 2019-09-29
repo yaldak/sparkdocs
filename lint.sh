6ed4d04f66ffb7ca7f29793db7128170ce56776f
yalda <yalda@kako.cc>
2019-09-29 01:48:58 -0500

    update linter to catch tabs

--- lint.sh
+++ lint.sh
@@ -3,8 +3,12 @@
 vale --glob='*.adoc' src/
 
 echo "Checking for trailing whitespace..."
-grep --color='auto' -r --include='*.adoc' '[[:blank:]]$' ./src/
+grep --color='auto' -r --include='*.adoc' '[[:blank:]]$' ./src
 
 # use `brew install pcre` to get pcregrep on Mac
+
+echo "Checking for tab characters..."
+pcregrep --include='.+\.adoc$' --color='auto' -r -n '\t' ./src
+
 echo "Checking for sketchy characters..."
-pcregrep --include='.+\.adoc$' --color='auto' -r -n '[^\x00-\x7F]' ./src/
+pcregrep --include='.+\.adoc$' --color='auto' -r -n '[^\x00-\x7F]' ./src
