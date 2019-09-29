9fb627bc922db24b4df88e673f3157b6d7d322de
yalda <yalda@kako.cc>
2019-09-29 01:46:30 -0500

    add a linter script

---
+++ lint.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+vale --glob='*.adoc' src/
+
+echo "Checking for trailing whitespace..."
+grep --color='auto' -r --include='*.adoc' '[[:blank:]]$' ./src/
+
+# use `brew install pcre` to get pcregrep on Mac
+echo "Checking for sketchy characters..."
+pcregrep --include='.+\.adoc$' --color='auto' -r -n '[^\x00-\x7F]' ./src/
