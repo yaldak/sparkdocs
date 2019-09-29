cd5bc63de915e64c5e9d8fa6c62574181a242d22
yalda <yalda@kako.cc>
2019-09-29 02:00:11 -0500

    improve the linter

--- lint.sh
+++ lint.sh
@@ -1,14 +1,39 @@
 #!/usr/bin/env bash
 
+# use `brew install pcre` to get pcregrep on Mac
+
+## Vale check
+echo "Running vale..."
 vale --glob='*.adoc' src/
 
+echo
+
+## Whitespace check
 echo "Checking for trailing whitespace..."
 grep --color='auto' -r --include='*.adoc' '[[:blank:]]$' ./src
 
-# use `brew install pcre` to get pcregrep on Mac
+if [ $? -eq 1 ]; then
+    echo "No trailing whitespace found."
+fi
 
+echo
+
+## Tab check
 echo "Checking for tab characters..."
 pcregrep --include='.+\.adoc$' --color='auto' -r -n '\t' ./src
 
-echo "Checking for sketchy characters..."
+if [ $? -eq 1 ]; then
+    echo "No tab characters found."
+fi
+
+echo
+
+## Non-ASCII check
+echo "Checking for non-ASCII characters..."
 pcregrep --include='.+\.adoc$' --color='auto' -r -n '[^\x00-\x7F]' ./src
+
+if [ $? -eq 1 ]; then
+    echo "No non-ASCII characters found."
+fi
+
+echo
